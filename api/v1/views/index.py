2494c95434e2443f21e045e8313280e787886804
WololoRC <wololoRC@proton.me>
2023-04-11 14:56:51 -0300

    fixes on paths from import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """index module"""
 from flask import jsonify
-from viwes import app_views
+from api.v1.views import app_views
 
-@app_views('/status', methods=['GET'])
+
+@app_views.route('/status', methods=['GET'])
 def status_route():
     return jsonify({'status': 'OK'})
