fc9be440c29e4b4940b6a39f1aadfa7522903bb1
WololoRC <wololoRC@proton.me>
2023-04-11 15:39:50 -0300

    added 'stats', now we know hoy many instances we have for any class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python3
-"""index module"""
+"""
+index module
+
+Methods
+-------
+status_route : None
+    Return API status
+
+stats_route : None
+    Return JSON file with <class>:<num_instances>>
+"""
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def status_route():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats_route():
+    """Return a json with all objects in storage"""
+
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User),
+        })
