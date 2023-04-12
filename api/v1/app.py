7d0e2df6170e21f3b1a23e794bf0efe2758a1e2d
JoaquinFurta <joaquinfurta2@gmail.com>
2023-04-12 05:07:27 -0700

    adding error_404_handler functionality

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """API module"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,13 @@ app.register_blueprint(app_views)
 def close_session(exeption):
     """Close current session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def err_handler(error):
+    """a handler for 404 errors that returns a JSON-formatted
+        404 status code response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
